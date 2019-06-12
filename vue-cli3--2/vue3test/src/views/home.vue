09176405341f42f898e84a835824cd55df0cf6b9
wendj <wendj@xiaomodo.com>
2019-06-12 16:14:25 +0800

    使用postcss-px-to-viewport 也可配置exclude

--- vue-cli3--2/vue3test/src/views/home.vue
+++ vue-cli3--2/vue3test/src/views/home.vue
@@ -2,7 +2,7 @@
   <div id="home-page">
     <div class="test">home</div>
     <img class="testImg" src="http://192.168.1.103:8489/s/group00/M00/00/19/ooYBAFz4vTGAPpyUAAw95Q5AXdE051.png?st=VRzfcZaZeJ_mqCba6Cz44g&e=1562399056" alt="">
-    <img class="testImg" src="../assets/dark.png" alt="">
+    <img class="testImg hairlines" src="../assets/dark.png" alt="">
     <div class="test-border"></div>
     <van-button type="danger" class="test-btn ignore">按钮</van-button>
     <van-button plain hairline type="danger">细边框按钮</van-button>
@@ -17,6 +17,7 @@
     <van-popup v-model="show" position="top" :overlay="false">
       内容
     </van-popup>
+    <div class="example"></div>
   </div>
 </template>
 
